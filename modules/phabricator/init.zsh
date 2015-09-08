e2e179b011a014fbf003a67441ca0d57fc270f88
Rhio Kim <rhio.kim@gmail.com>
2015-09-08 18:03:33 +0900

    update phabricator alias

--- modules/phabricator/init.zsh
+++ modules/phabricator/init.zsh
@@ -9,6 +9,10 @@
 if [[ -s "$HOME/.arc_install/arcanist/bin" ]]; then
   export PATH=$PATH:$HOME/.arc_install/arcanist/bin
 
-
-	alias garcb='arc branch'
+	alias arcb='arc branch'
+	alias arcd='arc diff'
+	alias arcl='arc land --onto'
+	alias arcp='arc patch'
+	alias arcll='arch list'
+	alias arct='arc tasks'
 fi

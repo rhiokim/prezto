9473419e0b45a90f096a1b369c87072ccabd2df8
Rhio Kim <rhio.kim@gmail.com>
2018-06-18 21:07:32 +0900

    fix(hyper): hyperterm comptability

--- init.zsh
+++ init.zsh
@@ -198,4 +198,14 @@ export NVM_DIR="$HOME/.nvm"
 export VISUAL=vim
 export EDITOR="$VISUAL"
 
+# Hyperterm Compatability (see https://github.com/zeit/hyperterm/issues/360)
+export LANG="en_US.UTF-8"
+export LC_COLLATE="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_MESSAGES="en_US.UTF-8"
+export LC_MONETARY="en_US.UTF-8"
+export LC_NUMERIC="en_US.UTF-8"
+export LC_TIME="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+
 [ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh

150c4128a247f1c858b0672920c47d5b009c970d
Rhio Kim <rhio.kim@gmail.com>
2020-02-23 22:07:53 +0900

    fix: set default vim -> nvm

--- aliases.zsh
+++ aliases.zsh
@@ -3,6 +3,9 @@ alias haroo='open -a "Haroopad"'
 alias code='open -a "Visual Studio Code"'
 alias codei='open -a "Visual Studio Code - Insiders"'
 
+alias vim="nvim"
+alias vi="nvim"
+
 # Application Demons
 
 # CouchDB
@@ -18,5 +21,5 @@ alias stop_mongo='launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.mongodb.
 # alias nw='/Users/rhio/Works/my/haroopad/lib/nwjs-v0.12.3-osx-x64/nwjs.app/Contents/MacOS/nwjs'
 # alias nw13='/Users/rhio/Works/my/haroopad/lib/nwjs-sdk-v0.13.4-osx-x64/nwjs.app/Contents/MacOS/nwjs'
 # alias nw16='/Users/rhio/Works/my/haroopad/lib/nwjs-sdk-v0.16.0-osx-x64/nwjs.app/Contents/MacOS/nwjs'
-alias nw20='/Users/rhio/Works/my/haroopad/lib/nwjs-sdk-v0.20.1-osx-x64/nwjs.app/Contents/MacOS/nwjs'
-alias nwjc='/Users/rhio/Works/my/haroopad/lib/nwjs-sdk-v0.17.6-osx-x64/nwjc'
+# alias nw20='/Users/rhio/Works/my/haroopad/lib/nwjs-sdk-v0.20.1-osx-x64/nwjs.app/Contents/MacOS/nwjs'
+# alias nwjc='/Users/rhio/Works/my/haroopad/lib/nwjs-sdk-v0.17.6-osx-x64/nwjc'
